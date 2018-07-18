7a62058afb57d5b73939eec5ba17348d2900bbb8
670894753@qq.com <670894753@qq.com>
2018-07-18 17:55:07 +0800

    no message

--- ToastDemo/HQActionSheet.swift
+++ ToastDemo/HQActionSheet.swift
@@ -9,6 +9,50 @@
 import Foundation
 import UIKit
 
+////这部分用于从plist文件中解析控制器
+let POST_HOST  = "http://192.168.2.5/"
+let ClassClass = "classClass";
+let TitleVC = "classNavigationTitle"//控制器标题
+let ClassImageName  = "classImageName"//控制器是tabBar的图片
+let SelectImage  = "classSelectedImageName"//控制器是tabBar的图片
+let AttachInfo  = "classSelectedImageName"//控制器 的附加信息
+
+let SCREEN_HEIGHT =  UIScreen.main.bounds.size.height
+let SCREEN_WIDTH = UIScreen.main.bounds.size.width
+
+let DidPaySuccessMessage = "didPaySuccessMessage"//支付成功通知标志
+let DidPayFailedMessage = "didPayFailedMessage"//失败通知标志
+
+let CurrentAppDelegate =  UIApplication.shared.delegate// 当前应用AppDelegate
+
+func IMG(name:String) -> UIImage? {//通过图片名快速生成图片
+    let image:UIImage? = UIImage.init(named: name)
+    return image
+}
+
+func UIColorFromHX(rgbValue:Int) -> UIColor?{//通过16进制颜色值生成颜色
+    
+    return UIColor.init(red:CGFloat(((Float)((rgbValue & 0xFF0000) >> 16))/255.0) , green: CGFloat(((Float)((rgbValue & 0xFF00) >> 8))/255.0) , blue: CGFloat(((Float)(rgbValue & 0xFF))/255.0) , alpha: 1.0)
+}
+func UIColorFromRGB(R:Int,G:Int,B:Int) -> UIColor?{//通过RGB生成颜色
+    return UIColor.init(red: CGFloat(R)/255.0, green: CGFloat(G)/255.0, blue: CGFloat(B)/255.0, alpha: 1.0)
+}
+
+func UIFontPFSC(fontSize:Int) ->UIFont?{//平方字体
+    
+    return UIFont.init(name: "PingFang SC", size: CGFloat(fontSize))
+}
+
+func ImageWithColor(color:UIColor) -> UIImage? {//颜色转图片
+    let rect:CGRect = CGRect.init(x: 0.0, y: 0.0, width:  1.0, height:  1.0)
+    UIGraphicsBeginImageContext(rect.size)
+    let context = UIGraphicsGetCurrentContext()
+    context?.setFillColor(color.cgColor)
+    context?.fill(rect)
+    let image = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+    return image
+}
 func SCREEN_ADJUST(value:CGFloat) -> CGFloat {
     return SCREEN_WIDTH * (value) / 375.0
 }
@@ -33,7 +77,6 @@ enum HQOtherActionItemAlignment:Int {
 
 class HQActionSheet:UIView{
     var cancelBtnBackColor:UIColor?  = UIColor.white
-    var otherActionItemAlignment:HQOtherActionItemAlignment?  = .HQOtherActionItemAlignmentCenter//Default is SROtherActionItemAlignmentCenter when no images. SROtherActionItemAlignmentLeft when there are images.
     var dissMissBlock:(()->())? = nil
     var didSelectSheetBlock:((_ index:NSInteger,_ actionSheet:HQActionSheet)->())? = nil
     
@@ -44,7 +87,7 @@ class HQActionSheet:UIView{
     var cancelTitle:NSString? = nil
     var destructiveTitle:NSString? = nil
     var otherTitles:NSArray? = nil
-    var otherImages:NSArray? = nil
+    var otherImages:NSArray? = nil//字符串或者UIImage
     
     var offsetY:CGFloat? = nil
     var actionSheetHeight:CGFloat? = nil
@@ -61,6 +104,37 @@ class HQActionSheet:UIView{
         return ImageWithColor(color: kActionItemHighlightedColor!)
     }()
     
+    var alignment:HQOtherActionItemAlignment? = .HQOtherActionItemAlignmentCenter
+    var otherActionItemAlignment:HQOtherActionItemAlignment?{
+        set{
+            self.alignment = newValue
+            switch  newValue {
+            case .HQOtherActionItemAlignmentLeft?:do {
+                for actionItem in self.otherActionItems!{
+                    let title = (actionItem as! UIView).viewWithTag(1) as! UILabel
+                    title.textAlignment = NSTextAlignment.left
+                    var newFrame:CGRect  = (actionItem as! UIView).frame
+                    newFrame.origin.x = 10
+                    (actionItem as! UIView).frame = newFrame
+                }
+                break
+                }
+            default:do {
+                for actionItem in self.otherActionItems!{
+                    let title:UILabel = (actionItem as! UIView).viewWithTag(1) as! UILabel
+                    title.textAlignment = NSTextAlignment.center
+                    var newFrame:CGRect  = (actionItem as! UIView).frame
+                    newFrame.origin.x = self.frame.size.width * 0.5 - newFrame.size.width * 0.5
+                    (actionItem as! UIView).frame = newFrame
+                }
+                break
+                }
+            }
+        }
+        get{
+            return self.alignment
+        }
+    } //Default is SROtherActionItemAlignmentCenter when no images. SROtherActionItemAlignmentLeft when there are images.
     class func actionSheet(title:NSString?,
                            cancelTitle:NSString?,
                            cancelBtnBackColor:UIColor?,
@@ -79,25 +153,22 @@ class HQActionSheet:UIView{
         sheet.frame = CGRect.init(x: 0, y: 0, width: SCREEN_WIDTH, height: SCREEN_HEIGHT)
         sheet.setupCover()
         sheet.setupActionSheet()
-//      sheet.cover?.backgroundColor = UIColor.red
-//      sheet.cover?.alpha = 1.0
-        sheet.alpha = 1.0
-        sheet.actionSheet?.frame =  CGRect.init(x: 0, y: sheet.frame.height, width: sheet.frame.width, height: sheet.offsetY!)
+        sheet.backgroundColor = UIColor.clear
+        sheet.actionSheet?.frame =  CGRect.init(x: 0, y: sheet.frame.maxY, width: sheet.frame.width, height: sheet.offsetY!)
         sheet.actionSheetHeight = sheet.offsetY
         return sheet
     }
     func setupCover() {
         let cover:UIView = UIView.init()
         cover.frame = self.bounds;
-        cover.backgroundColor =  UIColor.white
-        cover.backgroundColor?.withAlphaComponent(0.33)
-        let tap:UITapGestureRecognizer = UITapGestureRecognizer.init(target: self, action: #selector(dismiss))
+        cover.backgroundColor =  UIColor.black
+        cover.alpha = 0.1
+        let tap:UITapGestureRecognizer = UITapGestureRecognizer.init(target: self, action: #selector(cancelSheet(button:)))
         cover.addGestureRecognizer(tap)
         self.cover = cover;
-        self.cover?.alpha = 0;
         self.addSubview(self.cover!)
-        
     }
+    
     func setupActionSheet() {
         
         let actionSheet:UIView = UIView.init()
@@ -117,60 +188,78 @@ class HQActionSheet:UIView{
         
         self.setupCancelActionItem()
     }
+    
+    func setupTitleLabel()   {
+        if (self.title?.length ?? 0 > 0) {
+            let titleLabel:UILabel     = UILabel.init(frame: CGRect.init(x: 0, y: 0, width: self.frame.size.width, height: kActionItemHeight))
+            titleLabel.backgroundColor = UIColor.white
+            titleLabel.textColor       = kTitleColor
+            titleLabel.textAlignment   = NSTextAlignment.center
+            titleLabel.font            = UIFont.systemFont(ofSize: kTitleFontSize)
+            titleLabel.numberOfLines   = 0
+            titleLabel.text            = self.title! as String
+            self.actionSheet?.addSubview(titleLabel)
+            self.offsetY = kActionItemHeight + CGFloat(kLineHeight) + (self.offsetY ?? 0.0)
+        }
+    }
+    
     func setupOtherActionItems() {
         
-        if (self.otherTitles?.count ?? 0) > 0{
-            for i in 0..<(self.otherTitles?.count ?? 0)  {
-                
-                let otherBtn:UIButton  = UIButton.init(frame: CGRect.init(x: 0, y: self.offsetY!, width:  self.frame.size.width, height: kActionItemHeight))
-                otherBtn.backgroundColor = UIColor.white
-                otherBtn.tag = i
-                otherBtn.setBackgroundImage(self.normalImage, for: UIControlState(rawValue: UIControlState.normal.rawValue))
-                otherBtn.setBackgroundImage(self.normalImage, for: UIControlState(rawValue: UIControlState.highlighted.rawValue))
-                otherBtn.addTarget(self, action: #selector(didSelectSheet(button:)), for: UIControlEvents(rawValue: UIControlEvents.touchUpInside.rawValue))
-                self.actionSheet?.addSubview(otherBtn)
-                
-                let otherItem:UIView  = UIView.init()
-                otherItem.backgroundColor = UIColor.clear
-                let maxTitleSize:CGSize  = self.maxSizeInStrings(strings: self.otherTitles!)
-                if (self.otherImages?.count ?? 0) > 0{
-                    let icon:UIImageView   = UIImageView.init()
-                    icon.frame = CGRect .init(x: 0, y: 0, width: kActionItemHeight, height: kActionItemHeight)
-                    if (self.otherImages?.count ?? 0) > i{
-                        icon.image = (self.otherImages?[i] as! UIImage)
+        for i in 0..<(self.otherTitles?.count ?? 0)  {
+            
+            let otherBtn:UIButton  = UIButton.init(frame: CGRect.init(x: 0, y: self.offsetY!, width:  self.frame.size.width, height: kActionItemHeight))
+            otherBtn.backgroundColor = UIColor.white
+            otherBtn.tag = i
+            otherBtn.setBackgroundImage(self.normalImage, for: UIControlState(rawValue: UIControlState.normal.rawValue))
+            otherBtn.setBackgroundImage(self.normalImage, for: UIControlState(rawValue: UIControlState.highlighted.rawValue))
+            otherBtn.addTarget(self, action: #selector(didSelectSheet(button:)), for: UIControlEvents(rawValue: UIControlEvents.touchUpInside.rawValue))
+            self.actionSheet?.addSubview(otherBtn)
+            
+            let otherItem:UIView  = UIView.init()
+            otherItem.backgroundColor = UIColor.clear
+            let maxTitleSize:CGSize  = self.maxSizeInStrings(strings: self.otherTitles!)
+            if (self.otherImages?.count ?? 0) > 0{
+                let icon   = UIImageView.init()
+                icon.frame = CGRect.init(x: 0, y: 0, width: kActionItemHeight, height: kActionItemHeight)
+                if (self.otherImages?.count ?? 0) > i{
+                    let image = self.otherImages?[i]
+                    if image is UIImage{
+                         icon.image = (self.otherImages?[i] as! UIImage)
+                    }else if image is String{
+                        icon.image = UIImage.init(named: image as! String)
                     }
-                    icon.contentMode = UIViewContentMode.center
-                    icon.tag = 2
-                    otherItem.addSubview(icon)
-                    
-                    let title:UILabel  = UILabel.init(frame: CGRect.init(x: icon.frame.maxX, y: icon.frame.maxY, width: maxTitleSize.width, height: kActionItemHeight))
-                    
-                    title.font = UIFont.systemFont(ofSize: kActionItemFontSize)
-                    title.tintColor = UIColor.black
-                    title.text = (self.otherTitles?[i] as! String)
-                    title.tag = 1
-                    otherItem.addSubview(title)
-                    
-                    otherItem.frame = CGRect.init(x: 10, y: 0, width: kActionItemHeight + maxTitleSize.width, height: kActionItemHeight)
-                    
-                } else {
-                    let title:UILabel  = UILabel.init(frame: CGRect.init(x: 0, y: 0, width: maxTitleSize.width, height: kActionItemHeight))
-                    
-                    title.font = UIFont.systemFont(ofSize: kActionItemFontSize)
-                    title.tintColor = UIColor.black
-                    title.text = (self.otherTitles?[i] as! String)
-                    title.tag = 1
-                    otherItem.addSubview(title)
-                    otherItem.frame = CGRect.init(x: self.frame.size.width * 0.5 - maxTitleSize.width * 0.5, y: 0, width: maxTitleSize.width, height: kActionItemHeight)
                 }
+                icon.contentMode = UIViewContentMode.center
+                icon.tag = 2
+                otherItem.addSubview(icon)
                 
-                self.otherActionItems?.add(otherItem)
-                otherItem.isUserInteractionEnabled = false
-                if i == (self.otherTitles?.count ?? 0) - 1 {
-                    self.offsetY  = kActionItemHeight +  self.offsetY!
-                } else {
-                    self.offsetY  = kActionItemHeight + CGFloat(kLineHeight) + self.offsetY!
-                }
+                let title:UILabel  = UILabel.init(frame: CGRect.init(x: icon.frame.maxX, y: 0, width: maxTitleSize.width, height: kActionItemHeight))
+                
+                title.font = UIFont.systemFont(ofSize: kActionItemFontSize)
+                title.textColor = UIColor.black
+                title.text = (self.otherTitles?[i] as! String)
+                title.tag = 1
+                otherItem.addSubview(title)
+                
+                otherItem.frame = CGRect.init(x: 10, y: 0, width: kActionItemHeight + maxTitleSize.width, height: kActionItemHeight)
+                
+            } else {
+                let title:UILabel  = UILabel.init(frame: CGRect.init(x: 0, y: 0, width: maxTitleSize.width, height: kActionItemHeight))
+                title.font = UIFont.systemFont(ofSize: kActionItemFontSize)
+                title.textColor = UIColor.black
+                title.text = (self.otherTitles?[i] as! String)
+                title.tag = 1
+                otherItem.addSubview(title)
+                otherItem.frame = CGRect.init(x: self.frame.size.width * 0.5 - maxTitleSize.width * 0.5, y: 0, width: maxTitleSize.width, height: kActionItemHeight)
+            }
+            
+            self.otherActionItems?.add(otherItem)
+            otherBtn.addSubview(otherItem)
+            otherItem.isUserInteractionEnabled = false
+            if i == (self.otherTitles?.count ?? 0) - 1 {
+                self.offsetY  = kActionItemHeight +  self.offsetY!
+            } else {
+                self.offsetY  = kActionItemHeight + CGFloat(kLineHeight) + self.offsetY!
             }
         }
     }
@@ -194,19 +283,7 @@ class HQActionSheet:UIView{
             self.offsetY  = CGFloat(kActionItemHeight) + self.offsetY!
         }
     }
-    func setupTitleLabel()   {
-        if (self.title?.length ?? 0 > 0) {
-            let titleLabel:UILabel          = UILabel.init(frame: CGRect.init(x: 0, y: 0, width: self.frame.size.width, height: kActionItemHeight))
-            titleLabel.backgroundColor = UIColor.white
-            titleLabel.textColor       = kTitleColor
-            titleLabel.textAlignment   = NSTextAlignment.center
-            titleLabel.font            = UIFont.systemFont(ofSize: kTitleFontSize)
-            titleLabel.numberOfLines   = 0
-            titleLabel.text            = self.title! as String
-            self.actionSheet?.addSubview(titleLabel)
-            self.offsetY = kActionItemHeight + CGFloat(kLineHeight) + (self.offsetY ?? 0.0)
-        }
-    }
+    
     func setupCancelActionItem() {
         if self.cancelTitle != nil
         {
@@ -269,22 +346,27 @@ class HQActionSheet:UIView{
         if VC is UIWindow{
             let window:UIWindow =  VC as! UIWindow
             window.addSubview(self)
+            window.bringSubview(toFront: self)
         }
         else{
-            (VC as! UIViewController).view .addSubview(self)
+            (VC as! UIViewController).view.addSubview(self)
         }
   
         UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 0.9, initialSpringVelocity: 0.7, options: UIViewAnimationOptions.curveEaseInOut, animations: { [unowned self] in
-            self.cover?.alpha = 1.0;
-            self.actionSheet!.transform =   CGAffineTransform.init(translationX: 0, y: self.actionSheetHeight!)
+            self.cover?.alpha = 0.33 
+            self.actionSheet!.transform =
+                CGAffineTransform.init(translationX: 0, y: -self.actionSheetHeight!)
         }, completion: nil)
     }
     @objc func dismiss(){
         UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 0.9, initialSpringVelocity: 0.7, options: UIViewAnimationOptions.curveEaseOut, animations: {[unowned self] in
             self.cover?.alpha = 0.0;
+            self.backgroundColor = UIColor.clear
             self.actionSheet!.transform = CGAffineTransform.identity
         }) {[unowned self] (finished:Bool) in
-            self.removeFromSuperview()
+            if self != nil{
+                self.removeFromSuperview()
+            }
         }
     }
 }
--- ToastDemo/ViewController.swift
+++ ToastDemo/ViewController.swift
@@ -7,19 +7,31 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
-
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.white
+        
+        self.view.makeToast(message: "abc", duration: 5, position: CSToastPositionCenter as AnyObject, title: "可以", image: UIImage.init(named: "bg_zhlyjg"), style: CSToastStyle()) { (finish: Bool) in //这个方法是带图片的弹框
+            
+           self.view.makeToast(message: "完成")//只带文字弹框
+        }
+//        otherTitles otherImages:可已有值 ，不传值时 传Optional.none
+        let sheet:HQActionSheet =  HQActionSheet.actionSheet(title: "提示", cancelTitle: "取消", cancelBtnBackColor: UIColor.white, destructiveTitle: "cdf", otherTitles: ["abc","cdf"], otherImages:[UIImage.init(named: "bg_zhlyjg"),UIImage.init(named: "bg_zhlyjg")], selectSheetBlock: {[unowned self]  (index:NSInteger, sheet:HQActionSheet) in
+            //选择回调
+        }) { [unowned self] in // 检查列表
+            //取消回调 点按背景 或者取消按钮
+        }
+        sheet.otherActionItemAlignment = .HQOtherActionItemAlignmentCenter//可加可不加，这里用计算属性来调用
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1) {
+            sheet.show()
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-
-
+    } 
 }
 
